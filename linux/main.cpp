affa4980140720401ccf495920f48f6a0bd204ab
Emanuel Vintila <emanuel.vintila@aisec.fraunhofer.de>
2019-04-03 09:58:20 +0200

    lock icon now embedded wihtin the qt app

--- linux/main.cpp
+++ linux/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setWindowIcon(QIcon("./icon.jpeg"));
+    a.setWindowIcon(QIcon(":/lock.png"));
 
     MainWindow w;
     w.show();
--- linux/password_derivator.pro
+++ linux/password_derivator.pro
@@ -36,3 +36,6 @@ FORMS += \
         mainwindow.ui
 
 DISTFILES +=
+
+RESOURCES += \
+    resources.qrc
